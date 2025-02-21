247c820e4a2130c1877f28ad5d9ac042fe71c0a6
Cognibuild <cognibuild@noreply.codeberg.org>
2025-02-21 19:04:37 +0000

    Update ui/main.py
    removed public share

--- ui/main.py
+++ ui/main.py
@@ -77,7 +77,7 @@ def run():
         uii.ui_restart_server = False
         try:
             # Add share=True to make the app publicly accessible
-            ui.queue().launch(inbrowser=launch_browser, server_name=server_name, server_port=server_port, share=True, ssl_verify=ssl_verify, prevent_thread_lock=True, show_error=True)
+            ui.queue().launch(inbrowser=launch_browser, server_name=server_name, server_port=server_port, share=False, ssl_verify=ssl_verify, prevent_thread_lock=True, show_error=True)
         except Exception as e:
             print(f'Exception {e} when launching Gradio Server!')
             uii.ui_restart_server = True
